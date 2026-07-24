e4ae9510bd66745886e9fc18a86a0a175283dad0
Nexos-Exos <exolesx@gmail.com>
2026-07-24 11:09:27 -0300

    Update Recolor Text.lua

--- Recolor Text.lua
+++ Recolor Text.lua
@@ -1,6 +1,5 @@
 local MessageType = {}
-MessageType.__index = MessageType
-local Type_Colors = {
+local Colors = {
   Cyan = "<font color='rgb(0,255,255)'>",
   Red = "<font color='rgb(255,0,0)'>",
   Yellow = "<font color='rgb(255,215,0)'>"
@@ -10,16 +9,16 @@ local function Recolor_Text(Color, Text)
   return Color .. Text .. "</font>"
 end
 
-function MessageType:Success(Text)
-  return Recolor_Text(Type_Colors.Cyan, Text)
+function MessageType:Info(Text)
+  return print(Recolor_Text(Colors.Cyan, Text))
 end
 
 function MessageType:Error(Text)
-  return Recolor_Text(Type_Colors.Red, Text)
+  return print(Recolor_Text(Colors.Cyan, Text))
 end
 
 function MessageType:Warn(Text)
-  return Recolor_Text(Type_Colors.Yellow, Text)
+  return print(Recolor_Text(Colors.Cyan, Text))
 end
 
 return MessageType
